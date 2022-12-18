93339774a4b5ab235345fa722c1380531c7a0a83
Yagr Xu <yagrxu@gmail.com>
2022-12-18 09:47:22 +0800

    minor update

---
+++ packer-demo/aws-ubuntu.pkr.hcl
@@ -0,0 +1,32 @@
+packer {
+  required_plugins {
+    amazon = {
+      version = ">= 0.0.2"
+      source  = "github.com/hashicorp/amazon"
+    }
+  }
+}
+
+source "amazon-ebs" "ubuntu" {
+  ami_name      = "learn-packer-linux-aws"
+  instance_type = "t2.micro"
+  region        = "ap-northeast-1"
+  source_ami_filter {
+    filters = {
+      name                = "ubuntu/images/*ubuntu-xenial-16.04-amd64-server-*"
+      root-device-type    = "ebs"
+      virtualization-type = "hvm"
+    }
+    most_recent = true
+    owners      = ["099720109477"]
+  }
+  ssh_username = "ubuntu"
+}
+
+build {
+  name    = "learn-packer"
+  sources = [
+    "source.amazon-ebs.ubuntu"
+  ]
+}
+
--- xray-demo/sample-apps/world/src/main/java/demo/aws/devx/springcloud/sample/world/controller/WorldController.java
+++ xray-demo/sample-apps/world/src/main/java/demo/aws/devx/springcloud/sample/world/controller/WorldController.java
@@ -3,15 +3,24 @@ package demo.aws.devx.springcloud.sample.world.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Random;
+
 import org.apache.logging.log4j.LogManager;
 
 @RestController
 public class WorldController {
 
     private static final Logger logger = LogManager.getLogger(WorldController.class);
-    
+
     @GetMapping("hello")
     public String sayHello(){
+        Random random = new Random();
+        int upperbound = 10;
+        int int_random = random.nextInt(upperbound); 
+        if (int_random > 5){
+            throw new RuntimeException("sorry!");
+        }
         logger.info("hello world");
         return "world";
     }
